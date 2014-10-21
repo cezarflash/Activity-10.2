ff6b21eb844a9f43929f5e0f69f169ddcf8014e7
cezarflash <cezarflash@yahoo.com>
2014-10-21 14:49:13 -0400

    answers for 10.2

--- 10.2.py
+++ 10.2.py
@@ -2,16 +2,28 @@ __author__ = 'Ayla'
 
 
 #1. Create a list of numbers in random order.
-a=[5,3,1,2,4]
+a=[1,3,2,4]
 #2. Create a loop that starts at the first element of the list and goes to the second to last element. For each iteration
 #in the loop, if the element you are at and the next element are out of order swap them, otherwise leave them alone.
 #Print the new list.
-for number in a:
-        a.sort()
+#for number in a:
+
+
+for i in range(len(a)-1):
+    #a[i]+a[i+1]
+    temp=a[1]
+    a[1]=a[2]
+    a[2]=temp
+
+
 print(a)
+
 #3. What happens if you put the for loop from part 2 inside of another for loop that runs from 1 to the length of the list?
 
-for i in range(1,len(a)):
-    for i in a:
-        a.sort()
+for j in range(len(a)):
+    for number in range(len(a)-1):
+        #a[i]+a[i+1]
+        temp=a[1]
+        a[1]=a[2]
+        a[2]=temp
 print(a)
